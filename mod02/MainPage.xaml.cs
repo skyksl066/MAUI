0d2e92cbdc2a44d3d30fd86a52a6f009a5de7080
Alex Kuo <skyksl066@gmail.com>
2026-04-27 17:00:26 +0800

    init

---
+++ mod02/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace mod02
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_GetOSVersion(object sender, EventArgs e)
+        {
+            LblOSVersion.Text = $"Platform:{DeviceInfo.Current.Platform}, Version:{DeviceInfo.Current.VersionString}";
+        }
+    }
+}
